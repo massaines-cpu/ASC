bb5479cc6ed887285a7a823da09bdde6864818ba
inesmassa <inesurvivor@gmail.com>
2026-07-10 11:17:25 +0200

    dataloader

--- src/models/shallow_convnet.py
+++ src/models/shallow_convnet.py
@@ -1,4 +1,24 @@
+import torch
+import torch.nn as nn
+
 class ShallowConvNet(nn.Module):
+    def __init__(self, n_channels=61, n_times=501, embedding_dim=100):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv1d(n_channels, 32, kernel_size=25),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(1),
+            nn.Flatten(),
+            nn.Linear(32, embedding_dim)
+        )
+
     def forward(self, x):
-        embedding = ...
-        return embedding
+        return self.encoder(x)
+
+
+if __name__ == "__main__":
+    x = torch.randn(16, 61, 501)
+    model = ShallowConvNet()
+    z = model(x)
+    print(z.shape)
