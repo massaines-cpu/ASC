6cefe41b928e0bcb3fb5cf9c423e3248138f1065
inesmassa <inesurvivor@gmail.com>
2026-07-17 11:24:12 +0200

    bloc test

--- src/dataset/dataloader.py
+++ src/dataset/dataloader.py
@@ -45,9 +45,7 @@ def create_dataloaders(
     test_dyads,
     batch_size=8,
 ):
-    """
-    Crée les DataLoaders d'entraînement, validation et test.
-    """
+# Crée les dataLoaders d'entraînement, validation et test.
 
     train_table, validation_table, test_table = split_by_dyad(
         classification_table,
@@ -90,3 +88,38 @@ def create_dataloaders(
     )
 
     return train_loader, validation_loader, test_loader
+
+if __name__ == "__main__":
+
+    all_metadata = pd.read_csv(METADATA_PATH)
+
+    classification_table = prepare_classification_table(
+        all_metadata,
+        target_column="eyes_code",
+        allowed_classes=["YO", "YF"],
+        label_map={
+            "YO": 0,
+            "YF": 1,
+        },
+    )
+
+    train_loader, validation_loader, test_loader = create_dataloaders(
+        classification_table=classification_table,
+        dataset_root=DATASET_ROOT,
+        train_dyads=["J2", "J4", "J5", "J7"],
+        validation_dyads=["J10"],
+        test_dyads=["J15"],
+        batch_size=8,
+    )
+
+    eeg_a, eeg_b, labels = next(iter(train_loader))
+
+    print("Nombre de batchs train :", len(train_loader))
+    print("Nombre de batchs validation :", len(validation_loader))
+    print("Nombre de batchs test :", len(test_loader))
+
+    print("EEG A :", eeg_a.shape)
+    print("EEG B :", eeg_b.shape)
+    print("Labels :", labels.shape)
+
+    print("Labels du batch :", labels)
